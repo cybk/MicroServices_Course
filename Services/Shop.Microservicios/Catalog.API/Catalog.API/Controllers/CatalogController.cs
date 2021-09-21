42177299d0cb7722a69d7f6434a36c118efac4ca
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2021-09-21 18:52:13 -0500

    Added repository and first controller

---
+++ Services/Shop.Microservicios/Catalog.API/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public CatalogController(IProductRepository repo)
+            => this.productRepository = repo ?? throw new ArgumentNullException(nameof(ProductRepository));
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await productRepository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+    }
+}
