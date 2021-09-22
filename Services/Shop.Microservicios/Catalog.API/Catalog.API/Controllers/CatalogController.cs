93cc892522a941c89ff1ebf4c385c430813931dd
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2021-09-21 20:13:58 -0500

    Some changes to start using docker

--- Services/Shop.Microservicios/Catalog.API/Catalog.API/Controllers/CatalogController.cs
+++ Services/Shop.Microservicios/Catalog.API/Catalog.API/Controllers/CatalogController.cs
@@ -2,6 +2,7 @@
 using Catalog.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Catalog.API.Controllers
@@ -21,5 +22,30 @@ namespace Catalog.API.Controllers
             await productRepository.CreateProduct(product);
             return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+            => Ok(await productRepository.GetProducts());
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await productRepository.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+            => Ok(await productRepository.UpdateProduct(product));
+
+        [HttpDelete("{id:length(24)}", Name = "Delete")]
+        public async Task<IActionResult> DeleteProductById(string id) 
+            => Ok(await productRepository.DeleteProduct(id));
+
     }
 }
